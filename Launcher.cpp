83f6d1aaec72b46096939db47ec07c3a76dfcce6
Tyler Kassis <tylerLK2003@gmail.com>
2025-03-21 14:06:08 -0400

    Updates to the Operator and Launcher The Operator's augmente dinformation request has been corrected. The Launcher can launch the Operator in an independent terminal and any logged commands will apear in Logs.txt.

--- Launcher.cpp
+++ Launcher.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void killSubsystems();
+// void killSubsystems();
 
 int main()
 {
@@ -14,48 +14,54 @@ int main()
     */
 
     // COMPILATION AND EXECUTION OF TERMINALS IN WINDOWS (ADAPT TO QNX OS)
-    // try
-    // {
-    //     // Compile all of the subsystems
-    //     int visualDisplaySystem = system("g++ -o VisualDisplay.exe VisualDisplay.cpp");
-    //     int communicationSystem = system("g++ -o Communication.exe Communication.cpp");
-    //     int computerSystem = system("g++ -o Computer.exe Computer.cpp");
-    //     int radarSystem = system("g++ -o Radar.exe Radar.cpp");
-    //     if ((visualDisplaySystem != 0) || (communicationSystem != 0) || (computerSystem != 0) || (radarSystem != 0))
-    //     { // Null Pointer Exception
-    //         throw runtime_error("The subsystmes failed to compile... Null Pointer Exception");
-    //     }
-    //     else
-    //     {
-    //         cout << "All subsystems have compiled compiled..." << endl; // Test Statement
+    try
+    {
+        //     // Compile all of the subsystems
+        int operatorSystem = system("g++ -o Operator.exe Operator.cpp");
+        // int visualDisplaySystem = system("g++ -o VisualDisplay.exe VisualDisplay.cpp");
+        // int communicationSystem = system("g++ -o Communication.exe Communication.cpp");
+        // int computerSystem = system("g++ -o Computer.exe Computer.cpp");
+        // int radarSystem = system("g++ -o Radar.exe Radar.cpp");
+        if ((operatorSystem != 0) /*|| (visualDisplaySystem != 0) || (communicationSystem != 0) || (computerSystem != 0) || (radarSystem != 0)*/)
+        { // Null Pointer Exception
+            throw runtime_error("The subsystmes failed to compile... Null Pointer Exception");
+        }
+        else
+        {
+            // cout << "All subsystems have compiled compiled..." << endl; // Test Statement
 
-    //         int runVisualDisplaySystem = system("start cmd /k VisualDisplay.exe"); // Execute Visual Display Subsystem
-    //         if (runVisualDisplaySystem != 0)
-    //         { // Null Pointer Exception
-    //             throw runtime_error("VisualDisplay.exe failed to execute... Null Pointer Exception");
-    //         }
-    //         int runCommunicationSystem = system("start cmd /k Communication.exe"); // Execute Communication Subsystem
-    //         if (runCommunicationSystem != 0)
-    //         { // Null Pointer Exception
-    //             throw runtime_error("Communication.exe failed to execute... Null Pointer Exception");
-    //         }
-    //         int runComputerSystem = system("start cmd /k Computer.exe"); // Execute Computer Subsystem
-    //         if (runComputerSystem != 0)
-    //         { // Null Pointer Exception
-    //             throw runtime_error("Computer.exe failed to execute... Null Pointer Exception");
-    //         }
-    //         int runRadarSystem = system("start cmd /k Radar.exe"); // Execute Radar Subsystem
-    //         if (runRadarSystem != 0)
-    //         { // Null Pointer Exception
-    //             throw runtime_error("Radar.exe failed to execute... Null Pointer Exception");
-    //         }
-    //     }
-    // }
-    // catch (const exception &e)
-    // {
-    //     cerr << "An error occurred: " << e.what() << endl;
-    //     return -1;
-    // }
+            int runOperatorSystem = system("start cmd /k Operator.exe"); // Execute Operator Subsystem
+            if (runOperatorSystem != 0)
+            { // Null Pointer Exception
+                throw runtime_error("Operator.exe failed to execute... Null Pointer Exception");
+            }
+            // int runVisualDisplaySystem = system("start cmd /k VisualDisplay.exe"); // Execute Visual Display Subsystem
+            // if (runVisualDisplaySystem != 0)
+            // { // Null Pointer Exception
+            //     throw runtime_error("VisualDisplay.exe failed to execute... Null Pointer Exception");
+            // }
+            //         int runCommunicationSystem = system("start cmd /k Communication.exe"); // Execute Communication Subsystem
+            //         if (runCommunicationSystem != 0)
+            //         { // Null Pointer Exception
+            //             throw runtime_error("Communication.exe failed to execute... Null Pointer Exception");
+            //         }
+            //         int runComputerSystem = system("start cmd /k Computer.exe"); // Execute Computer Subsystem
+            //         if (runComputerSystem != 0)
+            //         { // Null Pointer Exception
+            //             throw runtime_error("Computer.exe failed to execute... Null Pointer Exception");
+            //         }
+            //         int runRadarSystem = system("start cmd /k Radar.exe"); // Execute Radar Subsystem
+            //         if (runRadarSystem != 0)
+            //         { // Null Pointer Exception
+            //             throw runtime_error("Radar.exe failed to execute... Null Pointer Exception");
+            //         }
+        }
+    }
+    catch (const exception &e)
+    {
+        cerr << "An error occurred: " << e.what() << endl;
+        return -1;
+    }
 
     return 0;
 }
